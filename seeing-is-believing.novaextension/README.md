e86d822e21b53d88fe0a9eb2a17b573511c772d1
Sergey Burtsev <sergey@burtsev.ru>
2022-08-11 02:16:37 +0300

    Initial commit

---
+++ seeing-is-believing.novaextension/README.md
@@ -0,0 +1,38 @@
+**Seeing Is Believing** evaluates Ruby code, recording the results of each line.<br>
+If you like Swift Playgrounds, you'll like SiB.
+
+![](https://raw.githubusercontent.com/SergeyBurtsev/nova-seeing-is-believing/main/seeing-is-believing.novaextension/images/example.gif)
+
+## Requirements
+
+[Ruby](https://www.ruby-lang.org/) >= 2.1
+
+`$ gem install seeing_is_believing`
+
+Verify it installed correctly by checking that you can run it:
+```
+$ seeing_is_believing -e '1 + 1'
+1 + 1  # => 2
+```
+
+## Usage
+
+To run Seeing Is Believing:
+
+- Select the **Editor → Seeing Is Believing** menu item; or
+- Open the command palette and type `Seeing Is Believing`
+
+Choose action:
+- Annotate all lines
+- Annotate Marked Lines
+- Remove annotations
+
+## Examples
+
+Watch the [video](http://vimeo.com/73866851).
+
+## Credit
+
+All credit goes to Josh Cheek and [seeing_is_believing](https://github.com/JoshCheek/seeing_is_believing) contributors.
+
+
---
+++ seeing-is-believing.novaextension/Scripts/main.js
@@ -0,0 +1,58 @@
+
+exports.activate = function() {
+    // Do work when the extension is activated
+}
+
+exports.deactivate = function() {
+    // Clean up state before the extension is deactivated
+}
+
+nova.commands.register("seeing-is-believing.annotateAllLines", (editor) => {
+    run(editor, [])
+});
+
+nova.commands.register("seeing-is-believing.annotateMarkedLines", (editor) => {
+    run(editor, ["--xmpfilter-style"])
+});
+
+nova.commands.register("seeing-is-believing.removeAnnotations", (editor) => {
+    run(editor, ["--clean"])
+});
+
+function run(editor, commandArgs) {
+    const documentSpan = new Range(0, editor.document.length);
+    const unformattedText = editor.document.getTextInRange(documentSpan);
+
+    const sibCommand = ["seeing_is_believing"]
+    const args = [...sibCommand, ...commandArgs]
+    const process = new Process("/usr/bin/env", {
+      cwd: nova.workspace.path,
+      args: args,
+      stdio: "pipe",
+    })
+    const writer = process.stdin.getWriter();
+    writer.ready.then(() => {
+      writer.write(unformattedText);
+      writer.close();
+    });
+
+    let out = ""
+    let err = ""
+
+    process.onStdout(function(line) {
+        out += line
+    })
+
+    process.onStderr(function(line) {
+        err += line
+    })
+
+    process.onDidExit(function(status) {
+        if (err.length > 0) return reject(err)
+        if (out.length == 0) return
+
+        editor.edit((edit) => edit.replace(documentSpan, out))
+    })
+
+    process.start()
+}
